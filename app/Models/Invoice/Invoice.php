3dad001f6676a89b8f395efc837e65b78dc18337
Efthakhar <efthakharbin@gmail.com>
2023-08-20 01:13:27 +0600

    Define Invoice Model relationship with party,creator and warehouse

--- app/Models/Invoice/Invoice.php
+++ app/Models/Invoice/Invoice.php
@@ -2,8 +2,12 @@
 
 namespace App\Models\Invoice;
 
+use App\Models\Party;
+use App\Models\Setting\Warehouse;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model
 {
@@ -12,5 +16,20 @@ class Invoice extends Model
     public function invoiceItems()
     {
         return $this->hasMany(InvoiceItem::class);
+    }
+
+    public function party()
+    {   
+        return $this->belongsTo(Party::class, 'party_id');
+    }
+
+    public function warehouse()
+    {   
+        return $this->belongsTo(Warehouse::class, 'warehouse_id');
+    }
+
+    public function creator()
+    {   
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
