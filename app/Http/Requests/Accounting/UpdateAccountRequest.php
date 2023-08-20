fa95bdec185c06a533340b8fe5f3d7d60546e41a
Efthakhar <efthakharbin@gmail.com>
2023-08-20 18:55:44 +0600

    Add custom request class to handle validation in updating account

---
+++ app/Http/Requests/Accounting/UpdateAccountRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Accounting;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class UpdateAccountRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return Gate::allows('update_account');
+    }
+
+    public function rules(): array
+    {
+        $account_id = $this->route('id');
+
+        return [
+            'name' => ['required', 'string', Rule::unique('accounts')->ignore($account_id)],
+            'bank_name' => ['string', 'required_with:branch_name'],
+            'branch_name' => ['string'],
+            'account_number' => ['required', 'string'],
+            'details' => ['string'],
+            'status' => ['in:active,disabled'],
+        ];
+    }
+}
