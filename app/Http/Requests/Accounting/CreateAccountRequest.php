a5133b66244c2410f730f8e55b2a6d18e7d39d83
Efthakhar <efthakharbin@gmail.com>
2023-08-20 18:51:20 +0600

    Add custom request class to handle account creation validation

---
+++ app/Http/Requests/Accounting/CreateAccountRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Accounting;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class CreateAccountRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return Gate::allows('create_account');
+    }
+
+    public function rules(): array
+    {
+
+        return [
+            'name' => ['required', 'string', Rule::unique('accounts')],
+            'bank_name' => ['string', 'required_with:branch_name'],
+            'branch_name' => ['string'],
+            'account_number' => ['required', 'string'],
+            'balance' => ['numeric'],
+            'details' => ['string'],
+            'status' => ['in:active,disabled'],
+        ];
+    }
+}
