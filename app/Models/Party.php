8025cb313293a9f285b2a5665d6b0e93ba54483e
Efthakhar <efthakharbin@gmail.com>
2023-08-21 19:36:18 +0600

    Add default due amounts of parties in Party model

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Party extends Model
 {
     use HasFactory;
+
+    protected $attributes = [
+        'sale_due' => 0,
+        'sale_return_due' => 0,
+        'purchase_due' => 0,
+        'purchase_return_due' => 0,
+    ];
 }
