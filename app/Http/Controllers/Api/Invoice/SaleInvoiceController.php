7a724125732f4743e3bf9e638fb27d179b9d5331
Efthakhar <efthakharbin@gmail.com>
2023-08-22 12:21:17 +0600

    Add Sale Invoice Controller

---
+++ app/Http/Controllers/Api/Invoice/SaleInvoiceController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api\Invoice;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Invoice\CreateSaleRequest;
+use App\Http\Resources\Invoice\SaleListResource;
+use App\Models\Invoice\Invoice;
+use App\Services\SaleService;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SaleInvoiceController extends Controller
+{
+    protected $saleService;
+
+    public function __construct(SaleService $saleService)
+    {
+        $this->saleService = $saleService;
+    }
+
+    public function store(CreateSaleRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $this->saleService->create($request->input());
+            DB::commit();
+
+        } catch (Exception $e) {
+
+            DB::rollback();
+
+            return response()->json([
+                'status' => 'error',
+                'message' => $e,
+            ], 500);
+        }
+
+    }
+
+    public function index(Request $request)
+    {
+        $this->authorize('view_sale');
+
+        $page = $request->query('page');
+        $per_page = $request->query('per_page') && $request->query('per_page') < 100 ? $request->query('per_page') : 10;
+        $search = $request->query('search');
+
+        $sales = Invoice::query();
+
+        $sales->when($search, function ($query, $search) {
+            $query->where('invoice_ref', 'LIKE', '%'.$search.'%');
+        });
+
+        $sales = $page ? $sales->orderBy('id', 'desc')->paginate($per_page)
+            : $sales->orderBy('id', 'desc')->get();
+
+        return SaleListResource::collection($sales);
+    }
+}
